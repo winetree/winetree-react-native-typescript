dcd4b619cf839b58820415d6594e27ec7edb8861
winetree <winetreee@gmail.com>
2019-05-21 00:21:50 +0900

    tslint setting

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@
 
 import React, {Component} from 'react';
 import {Platform, Text, View} from 'react-native';
-import styles from './styles';
+import styles from './styles'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -24,7 +24,7 @@ interface Props {
 }
 
 export default class App extends Component<Props> {
-  render() {
+  public render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
