dcbe3553f2b14ac390bced1c5b6fac5cdb054bf7
winetree <winetreee@gmail.com>
2019-05-25 13:26:26 +0900

    20190525

--- src/components/AlbumList.tsx
+++ src/components/AlbumList.tsx
@@ -5,15 +5,16 @@ import ResAlbum from '../model/res/ResAlbum';
 
 class AlbumList extends Component {
 
-  public componentDidMount(){
-    axios.get<ResAlbum[]>('https://rallycoding.herokuapp.com/api/music_albums').then(res => {
-      console.log(res);
-    }).catch(err => {
-      console.log(err);
-    });
+  public componentDidMount() {
+    axios.get<ResAlbum[]>('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(res => {
+        console.log(res);
+      }).catch(err => {
+        console.log(err);
+      });
   }
 
-  public render(){
+  public render() {
     return (
       <View>
         <Text>Album list</Text>
